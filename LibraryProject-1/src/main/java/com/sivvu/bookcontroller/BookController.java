1e6471335ca26da7956c354ac5f5ac2fb8b9dc27
sivabagyam <sivabagyam63@gmail.com>
2026-05-17 19:05:56 +0530

    book controller using spring boot

---
+++ LibraryProject-1/src/main/java/com/sivvu/bookcontroller/BookController.java
@@ -0,0 +1,32 @@
+package com.sivvu.bookcontroller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sivvu.bookmodel.Book;
+import com.sivvu.bookservice.BookService;
+
+@RestController
+@RequestMapping("/library")
+public class BookController {
+	private BookService service;
+	public BookController(BookService service) {
+		this.service=service;
+	}
+	
+	@GetMapping("/books")
+    public List<Book> getbooks(){
+		return service.getbooks();
+	}
+	
+	@PostMapping("/books")
+	public void addbooks(@RequestBody Book book) {
+	 service.addbooks(book);	
+	}
+}
